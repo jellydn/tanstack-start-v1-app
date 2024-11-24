7364d43bb6759c42d22a4cbd1dd319225e85b9bd
jellydn <jellydn@users.noreply.github.com>
2024-11-24 09:29:39 +0000

    Apply automatic changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
